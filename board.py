d91bf6fdaebe364e0bf9362e8a0b3b0834ecd18b
Aryan Sinha <sinharaks@gmail.com>
2022-07-19 14:52:12 -0400

    Stage 3: Tic Tac Toe complete functionality implemented

--- board.py
+++ board.py
@@ -1,5 +1,5 @@
-# Python Program for simple Tic Tac Toe
-
+# Python Program for Tic Tac Toe Game
+# Declare a list of lists
 board = [["-", "-", "-"],
          ["-", "-", "-"],
          ["-", "-", "-"]]
@@ -9,13 +9,23 @@ winner = None
 current_player = "Player 1"
 current_player_tic_pick = "X"
 
+# Function for Tic Tac Toe Game play
 def play_game():
     print_board()
 
     while game_still_going:
         handle_turn(current_player)
+        check_if_game_over()
         flip_player()
 
+    if winner == "X":
+        print("Player 1 wins!. Game over!")
+    elif winner == "O":
+        print("Player 2 wins!. Game over!")
+    elif winner is None:
+        print("it's a Tie!. Game draw!")
+
+# prints the current board
 def print_board():
     print("Printing board")
     print("['" + board[0][0] + "', '" + board[0][1] + "', '" + board[0][2] + "']")
@@ -23,18 +33,22 @@ def print_board():
     print("['" + board[2][0] + "', '" + board[2][1] + "', '" + board[2][2] + "']")
     print("\n")
 
+# checks if input is valid
+# Valid: if row number is between 0 and 2 and column number is also between 0 and 2
 def isvalidinput(rowposition, colposition):
     if(int(rowposition) >= 0 and int(rowposition) < 3 and int(colposition) >= 0 and int(colposition) < 3):
         return True
     else:
         return False
 
+# checks if row and column position is available
 def ispositionavailableinboard(rowposition, colposition):
     if (board[rowposition][colposition] == "-"):
         return True
     else:
         return False
 
+# function that handles players turn
 def handle_turn(player):
     print(player + ", make your move")
     rowposition = int(input("Enter row nos (0-2):"))
@@ -58,6 +72,27 @@ def handle_turn(player):
     board[rowposition][colposition] = current_player_tic_pick
     print_board()
 
+# Checks if the game is over
+def check_if_game_over():
+    check_for_winner()
+    check_for_tie()
+
+# Checks if there is a winner
+def check_for_winner():
+    global winner
+    row_winner = check_rows()
+    column_winner = check_columns()
+    diagonal_winner = check_diagonals()
+    if row_winner:
+        winner = row_winner
+    elif column_winner:
+        winner = column_winner
+    elif diagonal_winner:
+        winner = diagonal_winner
+    else:
+        winner = None
+
+# checks if the row has an empty data
 def check_rows():
     global game_still_going
     row_1 = board[0][0] == board[0][1] == board[0][2] != "-"
@@ -74,6 +109,7 @@ def check_rows():
     else:
         return None
 
+# checks if the column has an empty data
 def check_columns():
     global game_still_going
     column_1 = board[0][0] == board[1][0] == board[2][0] != "-"
@@ -90,6 +126,7 @@ def check_columns():
     else:
         return None
 
+# checks if the diagonal has an empty data
 def check_diagonals():
     global game_still_going
     diagonal_1 = board[0][0] == board[1][1] == board[2][2] != "-"
@@ -104,6 +141,17 @@ def check_diagonals():
     else:
         return None
 
+# checks if there is a tie
+def check_for_tie():
+    global game_still_going
+    if ("-" in board[0][0] or "-" in board[0][1] or "-" in board[0][2] or "-" in board[1][0] or "-" in board[1][1]
+            or "-" in board[1][2] or "-" in board[2][0] or "-" in board[2][1] or "-" in board[2][2]):
+        return False
+    else:
+        game_still_going = False
+        return True
+
+# Flips player from 1 to 2 or vice versa
 def flip_player():
     global current_player
     global current_player_tic_pick
@@ -115,4 +163,5 @@ def flip_player():
         current_player = "Player 1"
         current_player_tic_pick = "X"
 
+# call madse to play Tic Tac Toe game
 play_game()
